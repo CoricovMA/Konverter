79df721fbfd4e86c35fa8f95b2bdbe3456781e08
CoricovacMA <coricovacMA@gmail.com>
2020-12-22 02:18:06 -0500

    added specifics for scraping class

--- src/main/java/org/epubmaker/Request/ScrapeSpecifics.java
+++ src/main/java/org/epubmaker/Request/ScrapeSpecifics.java
@@ -1,5 +1,48 @@
 package org.epubmaker.Request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.IOException;
+import java.util.List;
+
 public class ScrapeSpecifics extends BaseObject {
 
+    @JsonProperty("nextTag")
+    private String nextTag = "";
+
+    @JsonProperty("titleTag")
+    private String titleTag = "";
+
+    @JsonProperty("mainTextTag")
+    private String mainTextTag = "";
+
+    @JsonProperty("multipleTextTags")
+    private boolean multipleTextTags = false;
+
+    @JsonProperty("textTags")
+    private List<String> textTags = null;
+
+    public ScrapeSpecifics fromJson(String json) throws IOException {
+        return objectMapper.readValue(json, ScrapeSpecifics.class);
+    }
+
+    public String getNextTag(){
+        return this.nextTag;
+    }
+
+    public String getTitleTag() {
+        return titleTag;
+    }
+
+    public String getMainTextTag() {
+        return mainTextTag;
+    }
+
+    public boolean isMultipleTextTags() {
+        return multipleTextTags;
+    }
+
+    public List<String> getTextTags() {
+        return textTags;
+    }
 }
