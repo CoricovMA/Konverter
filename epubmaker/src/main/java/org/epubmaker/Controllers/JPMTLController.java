1f2d6afd7c406a0e92a6c5932801ff67ff91168f
CoricovMA <coricovacMA@gmail.com>
2021-01-15 00:08:18 -0500

    deploying to heroku works

--- epubmaker/src/main/java/org/epubmaker/Controllers/JPMTLController.java
+++ epubmaker/src/main/java/org/epubmaker/Controllers/JPMTLController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class JPMTLController {
 
     private static final Logger logger = LogManager.getLogger(JPMTLController.class);
+    private static final Map<String, JPBook> store = new HashMap<>();
 
     @RequestMapping(
             value = "/{bookTitle}/{bookID}",
@@ -24,7 +27,7 @@ public class JPMTLController {
     )
     public void handleGetBook(
             HttpServletResponse response,
-            @PathVariable String bookTitle, @PathVariable String bookID){
+            @PathVariable String bookTitle, @PathVariable String bookID) {
 
 
         try {
@@ -32,12 +35,19 @@ public class JPMTLController {
 
             long start = System.currentTimeMillis();
 
-            JPBook book = RequestMaker.getChapters(bookID, bookTitle);
-            book.writeBook();
+            JPBook book;
+
+            if (!store.containsKey(bookID)) {
+                book = RequestMaker.getChapters(bookID, bookTitle);
+                book.writeBook();
+                store.put(bookID, book);
+            } else {
+                book = store.get(bookID);
+            }
 
             logger.info("Book retrieved in {}ms", (System.currentTimeMillis() - start));
 
-            response.setHeader( "Content-Disposition", String.format("attachment;filename=%s.epub",  bookTitle ));
+            response.setHeader("Content-Disposition", String.format("attachment;filename=%s.epub", bookTitle));
             response.setContentType("application/epub+zip");
             response.getOutputStream().write(book.getOutputStream().toByteArray());
             response.getOutputStream().flush();
