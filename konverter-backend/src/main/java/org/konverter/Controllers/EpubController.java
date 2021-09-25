eb025cf530ec0aa8d7ca9e395fa8800bc8e90abc
coricovMA <coricovacMA@gmail.com>
2021-09-25 12:34:42 -0400

    upgraded dependency versions, renamed files, changed project layout

---
+++ konverter-backend/src/main/java/org/konverter/Controllers/EpubController.java
@@ -0,0 +1,28 @@
+package org.konverter.Controllers;
+
+import org.konverter.epub.KonverterBook;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EpubController {
+
+    @RequestMapping(
+            value = "/convert/epub",
+            method = RequestMethod.POST,
+            produces = "application/epub+zip"
+    )
+    @ResponseBody
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<byte []> handleEpubUpload(KonverterBook generatedBook){
+
+        generatedBook.make();
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        String.format("attachment; filename=\"%s.epub\"", generatedBook.getBookTitle()))
+                .body(generatedBook.getFinalBook());
+    }
+}
